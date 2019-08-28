b0ff06b4750aa5ffa549bc8c106f3d864daad572
medrimonia <medrimonia@gmail.com>
2019-08-28 18:45:45 +0200

    adding labels drawing for TopView

--- src/hl_labelling/gtkmm/labelling_display_widget.cpp
+++ src/hl_labelling/gtkmm/labelling_display_widget.cpp
@@ -12,12 +12,14 @@ LabellingDisplayWidget::LabellingDisplayWidget(LabellingManager* manager, const 
 
 void LabellingDisplayWidget::annotateImg(const std::string& name)
 {
+  MultiCameraWidget::annotateImg(name);
+  // TODO refactor -> cf personal note (LH)
   uint64_t timestamp = sources[name].timestamp;
   hl_communication::VideoSourceID source_id = sources[name].source_id;
+  cv::Mat display_img = sources[name].display_image;
   if (labelling_manager->hasSource(source_id))
   {
     hl_communication::CameraMetaInformation camera_information;
-    cv::Mat display_img = sources[name].display_image;
     labelling_manager->exportCorrectedFrame(source_id, timestamp, &camera_information);
     field.tagLines(camera_information, &display_img, cv::Scalar(0, 0, 0), 1.0, 10);
     // Draw balls
@@ -46,6 +48,19 @@ void LabellingDisplayWidget::annotateImg(const std::string& name)
       }
       std::string text = std::to_string(entry.first.team_id()) + "," + std::to_string(entry.first.robot_id());
       cv::putText(display_img, text, robot_in_img, cv::FONT_HERSHEY_PLAIN, 1.0, robot_color, 1, cv::LINE_AA);
+    }
+  }
+  else if (isTopViewID(source_id))
+  {
+    for (const auto& entry : labelling_manager->getBalls(timestamp))
+    {
+      int ball_id = entry.first;
+      cv::Scalar ball_color = cv::Scalar(0, 0, 255);
+      const Eigen::Vector3d& ball_in_field = entry.second;
+      cv::Point ball_in_img = top_view_drawer.getImgFromField(manager.getField(), eigen2CV(ball_in_field));
+      cv::circle(display_img, ball_in_img, 2, ball_color, CV_FILLED, cv::LINE_AA);
+      cv::putText(display_img, std::to_string(ball_id), ball_in_img, cv::FONT_HERSHEY_PLAIN, 1.0, ball_color, 1,
+                  cv::LINE_AA);
     }
   }
 }
